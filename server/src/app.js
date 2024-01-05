acb4f3f9242c6758ec4151a93637373424152e12
Marcos Antonio Senn <marcossenn@gmail.com>
2024-01-05 17:54:07 -0300

    vercel deploy config

--- server/src/app.js
+++ server/src/app.js
@@ -1,55 +1,43 @@
-const express = require('express');
-const cokieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors');
-const routes = require('./routes/index');
-const {createServer} = require('http');
+const express = require("express");
+const cokieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const routes = require("./routes/index");
+const { createServer } = require("http");
 
 const server = express();
 const httpServer = createServer(server);
 
 server.use(cors());
-server.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
-server.use(bodyParser.json({limit: '50mb'}));
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cokieParser());
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 server.use((_req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Actualizar para que coincida con el dominio desde el cual se realizará la solicitud
-	res.header('Access-Control-Allow-Credentials', 'true');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept',
-	);
-	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-	next();
-});
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://localhost:5173",
 
-app.get('/', (req, res) => {
-	const htmlResponse = `
-    <html>
-      <head>
-        <title>API Poyecto</title>
-      </head>
-      <body>
-        <h1>API Poyecto</h1>
-        <p>/scrap</p>
-        <p>/dolarOficial</p>
-      </body>
-    </html>
-  `;
-	res.send(htmlResponse);
+  ); // Actualizar para que coincida con el dominio desde el cual se realizará la solicitud
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
 });
 
 // Configuración de rutas principales
-server.use('/', routes);
+server.use("/", routes);
 // Manejo de errores
 server.use((err, _req, res, _next) => {
-	// eslint-disable-line no-unused-vars
-	const status = err.status || 500;
-	const message = err.message || err;
-	console.error(err);
-	res.status(status).send(message);
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
 });
 // Exportación del servidor HTTP para ser utilizado en otros lugares
 module.exports = httpServer;
